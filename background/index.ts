77414503bcd8968f8ede2dba088f4a7114d01aeb
Thnderbread <david.kiiing1227@gmail.com>
2023-10-24 05:11:05 -0400

    Implemented logic for background script - contacting API and formatting response data. Still need to test.

--- background/index.ts
+++ background/index.ts
@@ -1,11 +1,19 @@
 import browser from "webextension-polyfill";
+import { HelixVideo } from "@twurple/api";
+import { ApiClient } from "@twurple/api/lib";
+import { ExtensionAuthProvider } from "@twurple/auth-ext/lib";
+import { HelixVideoMutedSegmentData, HelixVideoData, } from "@twurple/api/lib/interfaces/endpoints/video.external";
+
+let clientId;
+const authProvider = new ExtensionAuthProvider(clientId);
+const api = new ApiClient({ authProvider });
 
 type Message = {
   action: 'fetch',
   value: null
 }
 
-type ResponseCallback = (data: any) => void
+type ResponseCallback = <T>(data: T) => void
 
 async function handleMessage({action, value}: Message, response: ResponseCallback) {
   if (action === 'fetch') {
@@ -19,8 +27,57 @@ async function handleMessage({action, value}: Message, response: ResponseCallbac
   }
 }
 
+/**
+ * Given a vodId, attempt to retrieve the VOD's muted segments data.
+ * We then format that data by adding the endingOffset property 
+ * for skipping. We also sort the array to make sure the muted
+ * segments are in order.
+*/
+
+type MutedVodSegment = {
+  id: string | undefined,
+  title: string | undefined,
+  duration: number,
+  endingOffset: number,
+  startingOffset: number,
+}
+
+type Vod = HelixVideo | null
+
+function formatMutedSegmentsData(video: Vod, mutedSegments: HelixVideoMutedSegmentData[]): MutedVodSegment[] {
+  return mutedSegments
+    .map(segment => {
+    const formattedSegment: MutedVodSegment = {
+      id: video?.id,
+      title: video?.title,
+      duration: segment.duration,
+      startingOffset: segment.offset,
+      endingOffset: segment.offset + segment.duration
+    }
+    return formattedSegment;
+  })
+    .sort()
+}
+
+async function getMutedVodSegmentsFromTwitch(vodId: string): Promise<MutedVodSegment[] | undefined> {
+  const vod = await api.videos.getVideoById(vodId)
+  const mutedSegments = vod?.mutedSegmentData
+
+  // Checking for undefined here also covers if the vod
+  // variable is null. Can return undefined regardless 
+  // as there's no data to process.
+
+  return mutedSegments === undefined 
+    ? undefined 
+    : formatMutedSegmentsData(vod, mutedSegments);
+}
+
 // @ts-ignore
 browser.runtime.onMessage.addListener((msg, sender, response) => {
   handleMessage(msg, response);
   return true;
 });
+
+browser.runtime.onMessage.addListener((message) => {
+  return getMutedVodSegmentsFromTwitch(message.vodId);
+})
