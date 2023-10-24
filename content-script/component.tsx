ce25067c7a2a89af4c691a3d30467994833323e3
Thnderbread <david.kiiing1227@gmail.com>
2023-10-24 09:31:46 -0400

    Added some error handling for erroneous response from bg script. Implemented findNearestMutedSegment, listenForMutedSegments, and listenForSkipRequest functions.

--- content-script/component.tsx
+++ content-script/component.tsx
@@ -3,15 +3,32 @@ import browser from 'webextension-polyfill'
 
 // const EXPIRES_IN = 259_000_000
 
+type MutedVodSegment = {
+  id: string | undefined,
+  title: string | undefined,
+  duration: number,
+  endingOffset: number,
+  startingOffset: number,
+}
+
 export default () => {
   const [fact, setFact] = useState('Click the button to fetch a fact!')
   const [loading, setLoading] = useState(false)
-  const [mutedSegment, setMutedSegment] = useState({}) // stores the nearest muted segment.
-  const video = document.querySelector("video")
+  
+  const [skipData, setSkipData] = useState([]) // current VOD's muted segments
+  const [nearestSegment, setNearestSegment] = useState<MutedVodSegment>() // stores the nearest muted segment.
+
+  type skipMethod = "auto" | "manual"
+  const [defaultSkipMethod, setDefaultSkipMethod] = useState<skipMethod>("auto") // default skip method - user's preferred choice
+  const [currentSkipMethod, setCurrentSkipMethod] = useState<skipMethod>(defaultSkipMethod) // if a user scrubs
+  const [prevStartingOffset, setPrevStartingOffset] = useState(0) // for undos
+  const [listeningForSkips, setListeningForSkips] = useState(false);
+
+  const video = document.querySelector("video") as HTMLVideoElement
 
   // binary search?
   // findNearestMutedSegment():
-    // go through skipdata (forEach)
+    // go through skipdata (find)
     // is video.currentTime <= startingOffset?
       // return this segment.
     // is video.currentTime > startingOffset?
@@ -20,7 +37,9 @@ export default () => {
       // go to next segment, repeat steps.
 
     // video.seeked = handleSeek()
-      // wasListenForSkipRequest() called? - state var to set and read this (listeningForSkips). if true
+      // wasListenForSkipRequest() called?
+        // ^ - state var to set and read this (listeningForSkips) 
+        // ^ - should also be set to false when not in a muted segment. if true:
         // stop listening.
           // restore default skip method
           // set listeningForSkips = false.
@@ -38,7 +57,7 @@ export default () => {
         // setInterval (1s)
         // if video.currentTime >= nearestSegmentStart && video.currentTime < nearestSegmentEnd:
           // if skipMethod === manual:
-            // setSkipState(manual)
+            // setSkipState(manual) - doesn't make sense? already know manual method is being used?
             // listenForSkipRequest()
             // updateSkipSegments() || findNearestMutedSegment()?
           // else if skipMethod === auto:
@@ -50,12 +69,6 @@ export default () => {
     // retrieve VOD data & user skip method preference.
     // video.onplaying = listenForMutedSegments().
     // video.onseeked = handleSeek().
-
-  const [skipData, setSkipData] = useState([])
-
-  type skipMethod = "auto" | "manual"
-  const [defaultSkipMethod, setDefaultSkipMethod] = useState<skipMethod>("auto") // default skip method - user's preferred choice
-  const [currentSkipMethod, setCurrentSkipMethod] = useState<skipMethod>(defaultSkipMethod) // if a user scrubs
 
   // Update skipMethod in localStorage once the user toggles it
   // useEffect(() => {
@@ -102,9 +115,15 @@ export default () => {
        */
   
       if (userSettings?.settings?.vodData[vodID] === undefined) {
-        const skipData = await browser.runtime.sendMessage({ vodID })
+        const data = await browser.runtime.sendMessage({ vodID })
+
+        if (data instanceof Error) {
+          console.error(data);
+          return
+        }
+
         settings.vodData[vodID] = { 
-          skipData,
+          skipData: data,
           // exp: Date.now() + EXPIRES_IN
         }
   
@@ -126,9 +145,26 @@ export default () => {
     }
   }
 
-  function determineCurrentLocation() {
-    const video = document.querySelector("video")
-    return video?.currentTime
+  /**
+   * Looks for the muted segment closest to
+   * the video's current time. If no segment
+   * is found, the user has most likely passed all of them.
+   * @returns The segment or undefined.
+   */
+  function findNearestMutedSegment(skipData: MutedVodSegment[]): MutedVodSegment | undefined {
+    // don't bother skipping if a muted
+    // segment will end in 5 seconds.
+    const cutoffPoint = 5
+    
+    return skipData.find((segment) => {
+      if (video?.currentTime <= segment.startingOffset) {
+        return segment
+      } else if (video?.currentTime > segment.startingOffset) {
+        if (video?.currentTime < segment.endingOffset - cutoffPoint) {
+          return segment
+        }
+      }
+    })
   }
 
   // Function is only called by other functions 
@@ -137,16 +173,62 @@ export default () => {
   }
 
   /**
-   * Listen for a skip prompt when the skipMethod is set to manual.
+   * Listen for a muted segment during VOD playback.
    */
-  function listenForSkip(skipMethod: skipMethod) {
-    //
+  function listenForMutedSegments(skipMethod: skipMethod) {
+    while (!video.paused && !video.ended) {
+
+      setInterval(() => {
+        setNearestSegment(findNearestMutedSegment(skipData))
+
+        if (nearestSegment === undefined) { return }
+        
+        if (
+          video.currentTime >= nearestSegment.startingOffset 
+          && video.currentTime < nearestSegment.endingOffset) {
+            if (defaultSkipMethod === 'manual') {
+              // - this will probably have to be a promise for 
+              // .then chaining and stuff after its completion, 
+              // can also use reject() to abort the promise when 
+              // a user scrubs to a different location
+              // listenForSkipRequest()
+            } else {
+              video.currentTime = nearestSegment.endingOffset
+              setNearestSegment(findNearestMutedSegment(skipData));
+            }
+          }
+      }, 1000)
+    }
   }
 
   /**
    * Listen for an undo prompt after skipping.
    */
   function listenForUndo() {
+    //
+  }
+
+  /**
+   * Listen for a request to skip when dealing with
+   * 'manual' skip method. Only listen if the video is
+   * playing, to give the user a way to initiate a browser
+   * action (open extension popup) without having to skip.
+   * TODO: Check for when video is unpaused? Will we need to call this again?
+   */
+  function listenForSkipRequest() {
+    while (listeningForSkips && !video.paused) {
+      browser.browserAction.onClicked.addListener(() => {
+        if (currentSkipMethod === 'manual') {
+          // ! FIXME
+          video.currentTime = nearestSegment?.endingOffset as number
+          setNearestSegment(findNearestMutedSegment(skipData))
+          setListeningForSkips(false)
+        }
+      })
+    }
+  }
+
+  function handleSeek() {
     //
   }
 
