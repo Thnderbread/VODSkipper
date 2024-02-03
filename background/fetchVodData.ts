cb73bb97496cffcff055b5579549d431b6f69ffe
Bobby <109012458+Thnderbread@users.noreply.github.com>
2024-02-03 01:56:28 -0500

    Removed console.logs

--- background/fetchVodData.ts
+++ background/fetchVodData.ts
@@ -3,14 +3,6 @@ import type { ApiResponse, MutedSegmentResponse } from "../types"
 // ! use environment variable
 const BASE_URL = "http://localhost:8000/vodData/"
 
-// TODO: Fix tests: optimal response, bad response, no response
-// TODO: Fix popup - what is shown when no server response, no vod data for vod, etc?
-// TODO:  - Cases: Vod data found, no vod data found, no server response, server error
-// TODO:    - Solutions: Idk just ask content script for stuff on load. Add clear buttons for errors?
-// TODO: Get CA cert for server
-// TODO: Set up cd for this shit
-// TODO: Double check for any boilerplate / stock shit
-// TODO: Review firefox version - logo isn't right and extension might not be working
 /**
  * Given a vodID, attempt to retrieve the VOD's muted segments data.
  *
@@ -21,7 +13,6 @@ const BASE_URL = "http://localhost:8000/vodData/"
 export async function fetchVodData(
   vodID: string,
 ): Promise<MutedSegmentResponse> {
-  console.debug(`Fetching muted segments for ${vodID}`)
   const controller = new AbortController()
   const requestTimeout = setTimeout(() => {
     controller.abort()
@@ -33,18 +24,15 @@ export async function fetchVodData(
       console.error(`Code: ${response.status} | Text: ${response.statusText}`)
       return [new Error("Something went wrong with the server."), undefined]
     }
+
     const data: ApiResponse = await response.json()
     clearTimeout(requestTimeout)
 
     return [null, data.segments]
   } catch (error: unknown) {
     if ((error as Error).name === "AbortError") {
-      console.error("Request ran too long, aborting.")
       return [new Error("Server request timed out."), undefined]
-      // Check for TypeError to generalize across browsers.
-      // TypeError, in most cases, denotes bad or failed req to server.
     } else if ((error as Error).name === "TypeError") {
-      console.error("Couldn't contact server.")
       return [new Error("Couldn't contact server."), undefined]
     }
     console.error(`Uncaught error: ${error}`)
