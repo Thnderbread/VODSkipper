acdb3a7c6768eaceb775f70af6fc3b90aedfe3b2
Bobby <109012458+Thnderbread@users.noreply.github.com>
2024-03-18 03:37:03 -0400

    Adjusted to see logging output

--- mocks/runTest.cjs
+++ mocks/runTest.cjs
@@ -28,31 +28,31 @@ function runTest(testCommand, server = null) {
     })
 
     sub.stdout.on('data', (data) => {
-      if (data.includes('"spec" Reporter:')) {
-        inResultsBlock = true
-      }
+      logger.info(`\b[${wdioPrefix}] ${data.toString()}`)
+      // if (data.includes('"spec" Reporter:')) {
+      //   inResultsBlock = true
+      // }
 
-      if (inResultsBlock) {
-        logger.info(`\b[${wdioPrefix}] ${data.toString()}`)
-      }
+      // if (inResultsBlock) {
+      // }
 
-      if (data.includes('Spec Files:')) {
-        inResultsBlock = false
-      }
+      // if (data.includes('Spec Files:')) {
+      //   inResultsBlock = false
+      // }
     })
 
     sub.stderr.on('data', (data) => {
-      if (data.includes('"spec" Reporter:')) {
-        inResultsBlock = true
-      }
+      logger.error(`\b[${wdioPrefix}] ${data.toString()}`)
+      // if (data.includes('"spec" Reporter:')) {
+      //   inResultsBlock = true
+      // }
 
-      if (inResultsBlock) {
-        logger.error(`\b[${wdioPrefix}] ${data.toString()}`)
-      }
+      // if (inResultsBlock) {
+      // }
 
-      if (data.includes('Spec Files:')) {
-        inResultsBlock = false
-      }
+      // if (data.includes('Spec Files:')) {
+      //   inResultsBlock = false
+      // }
     })
 
     sub.on('close', (code) => {
